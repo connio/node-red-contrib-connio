1517e031763a06986d10b849ef7cb63afa40462d
EvanJ0hnson <ivangerasimow@gmail.com>
2019-12-02 23:31:22 +0300

    feat(*) Implement "NodeStatusManager" class

---
+++ src/node-status-manager.js
@@ -0,0 +1,37 @@
+class NodeStatusManager {
+  /**
+   * @param {Object} node NodeRED node
+   */
+  constructor(node) {
+    Object.assign(this, {
+      managedNode: node,
+      status: new Map(),
+    })
+  }
+
+  /**
+   * @param {...{ id: StatusType, message: StatusMessage, color: StatusColor }} status
+   */
+  register(...statusList) {
+    for (let { id, ...config } of statusList) {
+      this.status.set(id, config)
+    }
+  }
+
+  /**
+   * @param {string} id
+   */
+  set(id) {
+    let { message, color } = this.status.get(id);
+
+    this.managedNode.status({
+      fill: color,
+      shape: 'ring',
+      text: message,
+    });
+  }
+};
+
+module.exports = {
+  NodeStatusManager
+};
